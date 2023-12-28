612c218840fa11ac84c143c8b9afc31034c74add
Casey Ellett <casey@simplethread.com>
2023-12-28 09:59:34 -0500

    Change tokyonight orange to a light purple

--- lua/user/tokyonight.lua
+++ lua/user/tokyonight.lua
@@ -2,6 +2,6 @@ local tokyotheme = require('tokyonight')
 tokyotheme.setup({
   on_colors = function (colors)
     colors.fg_gutter = '#79809c'
-    colors.orange = '#0bb8d6'
+    colors.orange = '#ab80d1'
   end
 })
