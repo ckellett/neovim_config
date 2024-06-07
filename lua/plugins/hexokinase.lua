0cce6034f4d4883f5f7ac32a8f2efb2db8fa7b98
Casey Ellett <casey@simplethread.com>
2024-06-07 13:59:19 -0400

    Switch to vim-hexokinase from colorizer

---
+++ lua/plugins/hexokinase.lua
@@ -0,0 +1,8 @@
+return {
+  'rrethy/vim-hexokinase',
+  build = 'make hexokinase',
+  config = function()
+    vim.g.Hexokinase_optInPatterns = 'full_hex,rgb,rgba,hsl,hsla'
+    vim.g.Hexokinase_virtualText = '⬤'
+  end
+}
