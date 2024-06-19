828a1cfd3442bb6a34387d82bc8f0c27d908dcd0
Casey Ellett <casey@simplethread.com>
2024-06-19 10:32:19 -0400

    Add todo-comments plugin

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    -- PERF: Performance
+    -- HACK: Sneaky
+    -- FIX: There's a bug
+    -- TODO: This is a thing to be done
+    -- NOTE: This is the note and what-not
+    -- WARNING: Danger
+  }
+}
